55c287920dc840ad67b10b71190721d4f1d82258
ican0422 <137014330+ican0422@users.noreply.github.com>
2024-06-28 00:34:02 +0900

    [level 0] Title: 두 수의 차, Time: 0.02 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/0/120803. 두 수의 차/두 수의 차.java
+++ 프로그래머스/0/120803. 두 수의 차/두 수의 차.java
@@ -1,7 +1,12 @@
 class Solution {
     public int solution(int num1, int num2) {
-        int answer = 0;
-        answer = num1 - num2;
+        int answer = num1 - num2;
+        
+        if (answer < -50000) {
+            System.out.println("-50000 미만입니다.");
+        } else if (answer > 50000){
+            System.out.println("50000 이상입니다.");
+        }
         return answer;
     }
 }
