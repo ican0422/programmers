fb70d5f18536077cbcede278bcc2dff1ff6cc602
Pain <137014330+ican0422@users.noreply.github.com>
2024-08-09 09:21:15 +0900

    [level 1] Title: 소수 만들기, Time: 2.53 ms, Memory: 72.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int solution(int[] nums) {
+        int answer = 0;
+        int len = nums.length;
+        
+        for (int i = 0; i < len - 2; i++) {
+            for (int j = i + 1; j < len - 1; j++) {
+                for (int k = j + 1; k < len; k++) {
+                    if (isPrime(nums[i] + nums[j] + nums[k])) {
+                        answer++;
+                    }
+                }
+            }
+        }
+
+        return answer;
+    }
+    
+    private boolean isPrime(int num) {
+        if (num <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(num); i++){
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
