851f39faf6ac06441ed7e823830988f25bccc761
Pain <137014330+ican0422@users.noreply.github.com>
2024-08-10 15:19:40 +0900

    [level 1] Title: 덧칠하기, Time: 3.54 ms, Memory: 86.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+class Solution {
+    public int solution(int n, int m, int[] section) {
+        Arrays.sort(section);
+        int answer = 0;
+        int last = 0;
+        
+        for (int i = 0; i < section.length; i++) {
+            if(section[i] > last) {
+                answer++;
+                last = section[i] + m - 1;
+            }
+        }
+        
+        return answer;
+    }
+}
